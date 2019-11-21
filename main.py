bcafd1c05109dfe6b041cfd884bebc5a2ea69f93
Harley Whales <whales.harley@gmail.com>
2019-11-21 20:45:45 +1100

    Fixed message sending

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ async def echo(ctx):
 @bot.command()
 async def birthday(ctx, *args):
     if args[0] == "set":
-        await ctx.send(F"You passed the argument {args[0]}")
+        #await ctx.send(F"You passed the argument {args[0]}")
         d = datetime.date(2019, args[2], args[1])
         await ctx.send(F"The date you set is {d}")
 
