bb4f850ba4ce525a4ed2e0de275f5d838ddd7f54
Harley Whales <whales.harley@gmail.com>
2019-11-21 22:32:55 +1100

    Fucked up user shit and dates

--- main.py
+++ main.py
@@ -4,11 +4,34 @@ import datetime
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix="!")
+
 users = []
+
+class usr():
+    def __init__(self, user):
+        self.user = user
+        self.name = user.name
+    
+    def __hash__(self):
+        return hash(self.user.hash())
+
+    def __repr__(self):
+        return self.user.id
+    
+    def setBirthday(self, bday):
+        self.birthday = bday
+
+def addUser(user):
+    if usr(user) not in users:
+        users.append(usr(user))
 
 @bot.event
 async def on_ready():
     print(F"Logged in as {bot.user}")
+
+@bot.event
+async def on_message(message):
+    addUser(message.author)
 
 @bot.command()
 async def echo(ctx):
@@ -17,8 +40,12 @@ async def echo(ctx):
 @bot.command()
 async def birthday(ctx, *args):
     if args[0] == "set":
-        #await ctx.send(F"You passed the argument {args[0]}")
+        addUser(ctx.author)
         d = datetime.date(2019, int(args[2]), int(args[1]))
-        await ctx.send(F"The date you set is {d}")
+        for u in users:
+            if u.user == ctx.author:
+                u.setBirthday(d)
+
+        
 
 bot.run(os.environ.get("TOKEN", ""))
