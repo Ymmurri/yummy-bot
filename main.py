8c6301db59cf463b0bccfd4e40a72f5d33edf9bf
Harley Whales <whales.harley@gmail.com>
2019-11-21 20:29:16 +1100

    Testing birthday set with multi args and datetime

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import discord
 import os
+import datetime
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix="!")
+users = []
 
 @bot.event
 async def on_ready():
@@ -13,8 +15,10 @@ async def echo(ctx):
     await ctx.send(ctx.message.content)
 
 @bot.command()
-async def birthday(ctx, arg1):
-    if arg1 == "set":
-        await ctx.send(F"You passed the argument {arg1}")
+async def birthday(ctx, *args):
+    if args[0] == "set":
+        await ctx.send(F"You passed the argument {args[0]}")
+        d = datetime.date(2019, args[2], args[1])
+        await ctx.send(F"The date you set is {d}")
 
 bot.run(os.environ.get("TOKEN", ""))
